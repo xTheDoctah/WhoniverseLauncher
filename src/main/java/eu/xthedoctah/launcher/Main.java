ecadd2b8b073df0d9e51d0ac8a85348f5bb606c9
xTheDoctah <alessio.caldargi@gmail.com>
2018-08-16 19:23:12 +0200

    Mminor stuff

--- src/main/java/eu/xthedoctah/launcher/Main.java
+++ src/main/java/eu/xthedoctah/launcher/Main.java
@@ -3,11 +3,9 @@ package eu.xthedoctah.launcher;
 
 import eu.xthedoctah.launcher.settings.Settings;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -25,19 +23,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
         });
-        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                primaryStage.setX(event.getScreenX() - xOffset);
-                primaryStage.setY(event.getScreenY() - yOffset);
-            }
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
         });
         primaryStage.setTitle("Whoniverse Launcher");
         primaryStage.initStyle(StageStyle.UNDECORATED);
